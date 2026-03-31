bac7b5f9d43357af16b1df6b961e9e6fcef9946a
hungba2601 <hungba2601@gmail.com>
2026-03-31 09:36:38 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,28 @@
-const CACHE_NAME = 'viettlot-lucky-v1';
+const CACHE_NAME = 'vietlott-lucky-v1';
 const ASSETS = [
   './',
   './index.html',
   './style.css',
   './app.js',
-  './manifest.json'
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
     })
   );
 });
